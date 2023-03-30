0ed5a7d235ecb9237a7b6af249de23bc11d0f53d
andreitoledo <andreiltoledo@hotmail.com>
2023-03-30 15:41:12 -0300

    Ajuste na Startup

--- Startup.cs
+++ Startup.cs
@@ -41,16 +41,7 @@ namespace Altsystems.ControleDeContatos
             services.AddEntityFrameworkSqlServer()
                 .AddDbContext<BancoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            //services.AddDbContext<BancoContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDatabaseDeveloperPageExceptionFilter();
-
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<BancoContext>();
-            //services.AddControllersWithViews();
         }
-
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
