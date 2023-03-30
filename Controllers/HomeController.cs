3158e659b76013c609b46eefb81f13dc67bd990c
andreitoledo <andreiltoledo@hotmail.com>
2023-03-30 16:31:46 -0300

    Ajuste erro logger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,12 +11,12 @@ namespace Altsystems.ControleDeContatos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
